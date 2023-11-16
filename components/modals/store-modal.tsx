e6bbe72e2cfa69e9f49ab352627442e3db4b37e4
devandrewjpn <yuutoandrew.jpn@gmail.com>
2023-11-16 11:01:51 -0300

    feat: setting dashboard route with storeid from prisma, and dealing with user authentication redirect

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -40,8 +40,8 @@ export const StoreModal = () => {
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       setLoading(true);
-      const response = await axios.post("/api/stores", values);
-      toast.success("Store created");
+      const response = await axios.post("/api/stores", values)
+      window.location.assign(`/${response.data.id}`)
     } catch (err) {
       toast.error("Something went wrong.");
     } finally {
